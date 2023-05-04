4814e96da7cb9a9cb4bca765d05c1a2274c7f944
Elijah Briggs <91144217+ElijahBriggs@users.noreply.github.com>
2023-05-04 09:06:48 -0500

    Just keep going!

---
+++ app/src/main/java/com/example/bransonlandingappas/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.example.bransonlandingappas
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.cardview.widget.CardView;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.content.Intent;
+import android.view.View;
+import android.widget.AdapterViewFlipper;
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
